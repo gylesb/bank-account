d5379018d640ac5622baf96d9b94adfb3548575b
Daniel Cheng and Gyles Batara <daniel@>
2017-09-26 16:44:48 -0700

    Complete logic function

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,14 @@ function Account(name, initDeposit) {
   this.accountName = name;
   this.accountBal = initDeposit;
 }
-// debugger;
-// Account.prototype.createAccount = function() {
-//   alert('HI');
-// }
+
+Account.prototype.Deposit = function(amount) {
+  return this.accountBal += amount;
+}
+
+Account.prototype.Withdraw = function(amount) {
+  return this.accountBal -= amount;
+}
 
 $(document).ready(function() {
   $("#register").submit(function(event) {
@@ -14,13 +18,44 @@ $(document).ready(function() {
 
     var AccountHolder = new Account(accName, accDeposit);
 
-    $("ul#account").append("<li><span class='accountName'>" + AccountHolder.accountName + "</span></li>");
+    $("ul#account").append("<li><span class='accountName'><input type='radio' name='account' value='"+AccountHolder.accountName+"'> " + AccountHolder.accountName + "</span></li>");
+
+    $("input#name").val("");
+    $("input#initialDeposit").val("");
 
     $(".accountName").last().click(function() {
-      console.log(parseFloat(AccountHolder.accountBal));
-      $("#balanceBox").text(AccountHolder.accountBal);
+      $("#balanceBox h3").text("You have $" + AccountHolder.accountBal +".00");
+      customer = AccountHolder.accountName;
     });
+    $("#reset").click(function() {
+      $("h3#balanceBox").empty();
+    });
+    // amount = parseFloat($("#depositAmt").val());
+    // console.log(AccountHolder.accountName);
+    // console.log(AccountHolder.Deposit(amount));
+    // AccountHolder.Deposit(amount);
 
+    $("#btnDeposit").click(function(event) {
+      var amount = parseFloat($("#depositAmt").val());
+      $("input:radio[name=account]:checked").each(function() {
+        if (AccountHolder.accountName === customer) {
+          // latestBal = AccountHolder.Deposit(amount);
+          $("#balanceBox h3").text("You have $" + AccountHolder.Deposit(amount)+".00");
+        }
+        event.preventDefault();
+      });
+    });
+    $("#btnWithdraw").click(function(event) {
+      var amount = parseFloat($("#withdrawalAmt").val());
+      $("input:radio[name=account]:checked").each(function() {
+        if (AccountHolder.accountName === customer) {
+          // AccountHolder.Withdraw(amount);
+          $("#balanceBox h3").text("You have $" + AccountHolder.Withdraw(amount) +".00");
+        }
+        event.preventDefault();
+      });
+    });
     event.preventDefault();
   });
+
 });
